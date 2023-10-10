3a999606488d8e28936646695a33fff6d911acd1
Thales de Oliveira Lacerda <63318165+LacThales@users.noreply.github.com>
2023-10-10 20:25:44 -0300

    Update PapelMoeda.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -11,10 +11,14 @@ public class PapelMoeda {
     }
 
     public int getValor() {
+        
         return valor;
+        
     }
 
     public int getQuantidade() {
+        
         return quantidade;
+        
     }
 }
