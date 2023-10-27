b6b2b9db1c12d6a78171420b9d44e966e6208772
Thales de Oliveira Lacerda <63318165+LacThales@users.noreply.github.com>
2023-10-27 18:33:05 -0300

    adicionando teste final

--- Source-Code-Inspection/src/br/calebe/ticketmachine/test/TrocoTest.java
+++ Source-Code-Inspection/src/br/calebe/ticketmachine/test/TrocoTest.java
@@ -9,7 +9,31 @@ import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 
 
 public class TrocoTest {
-    
+
+    @Test
+    public void testTrocoIterator() {
+        Iterator<PapelMoeda> iterator = troco.getIterator();
+
+        assertTrue(iterator.hasNext());
+        assertEquals(100, iterator.next().getValor());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(50, iterator.next().getValor());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(20, iterator.next().getValor());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(10, iterator.next().getValor());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(5, iterator.next().getValor());
+
+        assertTrue(iterator.hasNext());
+        assertEquals(2, iterator.next().getValor());
+
+        assertFalse(iterator.hasNext());
+    }
 
     // ============================ Cenários de Sucesso ======================== //
     @Test
